6471d339f68449317e4a2d3f91701aad97ab7744
mukumbasar <mehmetutkununoyunlari@gmail.com>
2024-05-07 22:40:48 +0300

    feat: Add CrossroadsDbContext

---
+++ Core/Crossroads.Domain/EntityConfigurations/Interfaces/IEntityTypeConfiguration.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crossroads.Domain.EntityConfigurations.Interfaces
+{
+    public interface IEntityTypeConfiguration
+    {
+    }
+}
---
+++ Infrastructure/Crossroads.Persistence/Context/CrossroadsDbContext.cs
@@ -0,0 +1,33 @@
+﻿using Crossroads.Domain.Entities.Bases;
+using Crossroads.Domain.EntityConfigurations.Interfaces;
+using Crossroads.Domain.Enums;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crossroads.Persistence.Context
+{
+    public class CrossroadsDbContext : IdentityDbContext<IdentityUser, IdentityRole, string>
+    {
+        public CrossroadsDbContext(DbContextOptions<CrossroadsDbContext> options) : base(options)
+        {
+            
+        }
+
+        //DbSets:
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(IEntityTypeConfiguration).Assembly);
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
